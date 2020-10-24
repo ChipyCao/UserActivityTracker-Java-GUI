677ff92cf409bf99a0ce6df97979efae0e367e23
Ciprian Aninoiu <cipi.aninoiu@yahoo.ro>
2020-10-25 00:31:14 +0300

    Delete Admin.java

--- TookTech/src/Admin.java
+++
@@ -1,7 +0,0 @@
-
-public class Admin extends User{
-	public Admin(Integer id, String password, String username, Integer priviledges) {
-		super(id, password, username, priviledges = 1);
-		/// Inherits all the User class stuff and also has priviledges turned on
-	}
-}
